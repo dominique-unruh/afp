5b86e427d1f1cf9208f3b63a2909661d1987c59f
Dominique Unruh <unruh@ut.ee>
2022-01-19 13:51:35 +0200

    Improved script

--- sync.sh
+++ sync.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+if ! git diff-index --quiet HEAD --; then
+    echo "Changed files in worktree, not syncing. (git filter-repo resets the worktree)"
+    exit 1
+fi
 
 for repo in afp-devel afp-2021-1 afp-2021 afp-2020 afp-2019; do
-    git fetch origin refs/notes/hg
-    git fetch "$repo" "branches/default:$repo"
-    git filter-repo --force --prune-empty always --refs refs/notes/hg
-    git push origin "$repo"
-    git push origin refs/notes/hg
+    echo "** $repo **"
+    git fetch --quiet origin refs/notes/hg
+    git fetch --quiet "$repo" "branches/default:$repo"
+    git filter-repo --quiet --force --prune-empty always --refs refs/notes/hg >/dev/null
+    git push --quiet origin "$repo"
+    git push --quiet origin refs/notes/hg
+    echo
 done
-
