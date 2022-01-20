8fbc8650f41bffa635c2ac7d927e77728d595a50
Dominique Unruh <unruh@ut.ee>
2022-01-20 09:37:02 +0200

    Fixed how sync.sh fetches notes

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ fi
 
 for repo in afp-devel afp-2021-1 afp-2021 afp-2020 afp-2019; do
     echo "** $repo **"
-    git fetch --quiet origin refs/notes/hg
+    git fetch --quiet origin refs/notes/hg:refs/notes/hg
     git fetch --quiet "$repo" "branches/default:$repo"
     git filter-repo --quiet --force --prune-empty always --refs refs/notes/hg >/dev/null
     git push --quiet origin "$repo"
