62f41011c01a380dd5a7151ef5eef572654aabb9
Dominique Unruh <unruh@ut.ee>
2022-02-04 18:50:46 +0200

    Check of dirty worktree: include additional checked out worktrees.

--- sync.sh
+++ sync.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-if ! git diff-index --quiet HEAD --; then
-    echo "Changed files in worktree, not syncing. (git filter-repo resets the worktree)"
-    exit 1
-fi
+git worktree list --porcelain | while read DIR; do
+    case "$DIR" in
+	worktree\ *) DIR="${DIR#worktree }"
+		     if ! git -C "$DIR" diff-index --quiet HEAD --; then
+			 echo "Changed files in worktree $DIR, not syncing. (git filter-repo resets the worktree)"
+			 exit 1
+		     fi
+		     ;;
+    esac
+done
 
 for repo in afp-devel afp-2021-1 afp-2021 afp-2020 afp-2019; do
     echo "** $repo **"
