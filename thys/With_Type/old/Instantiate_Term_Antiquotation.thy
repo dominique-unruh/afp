81a89044b8acf9c2b3627dc56f394ddbd8fd3aa5
Dominique Unruh <git.afp.vctclk@rwth.unruh.de>
2025-06-25 18:05:00 +0200

    Deleted With_Type/old

--- thys/With_Type/old/Instantiate_Term_Antiquotation.thy
+++
@@ -1,9 +0,0 @@
-theory Instantiate_Term_Antiquotation
-  imports Pure
-begin
-
-ML_file \<open>instantiate_term_antiquotation.ML\<close>
-
-setup \<open>ML_Context.add_antiquotation \<^binding>\<open>Term\<close> true Instantiate_Term_Antiquotation.antiquotation_Term\<close>
-
-end
--- thys/With_Type/old/Rewrite_Proofs.thy
+++
@@ -1,7 +0,0 @@
-theory Rewrite_Proofs
-  imports Main
-begin
-
-ML_file \<open>rewrite_proofs.ML\<close>
-
-end
